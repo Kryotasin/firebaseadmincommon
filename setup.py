0203b71000f712a5ff1beb6467164fe96b055aaa
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-04-13 04:14:31 -0700

    updates

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'djangofirebaseadmin',         # How you named your package folder (MyLib)
+  name = 'firebaseadmincommon',         # How you named your package folder (MyLib)
   #packages = ['djangofirebaseadmin'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Package to integrate Firebase Admin with Django.',   # Give a short description about your library
+  description = 'Package to wrap Firebase Admin with Python.',   # Give a short description about your library
   author = 'Kshitij Sinha',                   # Type in your name
-  author_email = 'kshitij.sinha94@domain.com',      # Type in your E-Mail
+  author_email = 'kshitij.sinha94@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Kryotasin/firebaseadmincommon',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Kryotasin/firebaseadmincommon/archive/0.1.tar.gz',    # I explain this later on
-  keywords = ['Django', 'firebase', 'admin'],   # Keywords that define your package best
+  download_url = 'https://github.com/Kryotasin/firebaseadmincommon/archive/0.3.tar.gz',    # I explain this later on
+  keywords = ['Python', 'firebase', 'admin'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'firebase_admin',
       ],
